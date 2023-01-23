64d10fff8f7cb2212a641c5039d6720832735dd4
Debora <deboraribeiro9015@gmail.com>
2023-01-23 17:53:47 -0300

    Captando evento de click

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //function criarPessoa() {}
 let pessoas = [];
-let pessoa =  {
+/* let pessoa =  {
     codigo :"001",
     nome : "Maria",
     nascimento : "12/08/2000",
@@ -10,21 +10,22 @@ let pessoa =  {
     cpf : "00000000000",
     especial : "nao",
     tipodepessoa : "fisica",
-    }
+    } */
+
 
 document.getElementById("botao-cadastro").addEventListener("click", function() {
     let form = document.getElementById("form");
     console.log("captou o click !");
-    pessoa =  {
-        codigo : form.codigo.value,
-        nome : form.nome.value,
-        nascimento : form.nascimento.value,
-        sexo : form.sexo.value,
-        altura : form.altura.value,
-        peso : form.peso.value,
-        cpf : form.cpf.value,
-        especial : form.especial.value,
-        tipodepessoa : form.tipodepessoa.value,
+    let pessoa =  {
+    codigo : form.codigo.value,
+    nome : form.nome.value,
+    nascimento : form.nascimento.value,
+    sexo : form.sexo.value,
+    altura : form.altura.value,
+    peso : form.peso.value,
+    cpf : form.cpf.value,
+    especial : form.especial.value,
+    tipodepessoa : form.tipodepessoa.value,
     }
     console.log(pessoa,"objeto")
     pessoas.push(JSON.stringify(pessoa));
